abf98f3b26f4691058265b3e53d1541fd406de7d
Caseres <maxy_caseres4@hotmail.com>
2022-01-26 23:28:42 -0300

    minor changes

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./Product.scss"
-import AddToCart from "../../assets/addToCart.png"
 
 
 export const Product: React.FC<any> = (props): any => {
 
     return <div className="product-card">
         <div className="off"> -20% </div>
-        <img className="product-img" src={props.product.data.src}></img>
+        <img alt="" className="product-img" src={props.product.data.src}></img>
         <div className="name-container"><div className="sub-texto">sub texto saraza</div><div>{props.product.data.name}</div></div>
         <div className="price-container"> <div className="prebius-price">$12312</div>${props.product.data.price}</div>
         {/* <button onClick={() => { props.addToCart(props.product.data) }} className="add-to-cart">
