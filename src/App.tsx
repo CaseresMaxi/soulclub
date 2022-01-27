049dc131753c6bfa720b50208127fa3db5ee7d70
Caseres <maxy_caseres4@hotmail.com>
2022-01-27 08:20:40 -0300

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import { Home } from './pages/Home/Home';
 export const App: React.FC<any> = (): any => {
 
   return (
-    <Router basename="/souldclub">
+    <Router >
       <Switch>
         <Route path="/" component={Home} exact>
         </Route>
