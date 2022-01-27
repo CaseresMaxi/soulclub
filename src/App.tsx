11fb892cc66c3cf8859ad2b628d9dc08842a8a83
Caseres <maxy_caseres4@hotmail.com>
2022-01-26 23:21:19 -0300

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,9 @@ export const App: React.FC<any> = (): any => {
       <Switch>
         <Route path="/" component={Home} exact>
         </Route>
-        <Route path="/add" component={AddProduct} exact>
+        <Route path="/add" component={AddProduct}>
         </Route>
-        <Route path="/add/:id" component={AddProduct} exact>
+        <Route path="/add/:id" component={AddProduct}>
         </Route>
       </Switch>
     </Router>
